ac3791470fd0d93ebf084ee98c23a874c5032ec4
mikerspencer <spencer.mike.r@gmail.com>
2018-04-27 14:38:37 +0100

    updated to get unsplit polygon areas

--- grass_analysis.R
+++ grass_analysis.R
@@ -16,11 +16,23 @@ system("v.rast.stats map=terrain_parish@hill_farms raster=Terrain50_slope@hill_f
 f = list.files("/home/mspencer/Cloud/Michael/SRUC/hill_farms/data/spatial", pattern="*.shp$")
 f = substr(f, 1, nchar(f) - 4)
 
+# Get total area of each vector
+lapply(f, function(i){
+   system(paste0("v.db.addcolumn map=", i, "@hill_farms columns='", i, "_t_area DOUBLE PRECISION'"))
+   system(paste0("v.to.db map=", i, "@hill_farms option=area columns=", i, "_t_area"))
+})
+
+# Temp!
+# Extract data
+lapply(f, function(i){
+   system(paste0("v.out.ogr input=", i, "@hill_farms output=", normalizePath("~"), "/Cloud/Michael/SRUC/hill_farms/data/spatial-processed/", i, "_temp.csv format=CSV"))
+})
+
 lapply(f, function(i){
    system(paste0("v.overlay ainput=", i, "@hill_farms binput=ag_parishes@hill_farms operator=and output=", i, "_parishes olayer=1,1,1"))
 })
 
-# Get area of each vector
+# Get sub area of each cut vector
 lapply(f[-1], function(i){
    system(paste0("v.db.addcolumn map=", i, "_parishes@hill_farms columns='", i, "_area DOUBLE PRECISION'"))
    system(paste0("v.to.db map=", i, "_parishes@hill_farms option=area columns=", i, "_area"))
