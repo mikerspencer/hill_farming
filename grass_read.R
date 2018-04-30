0065e3c508208d72ccf114b096b7f7d6d86664ea
mikerspencer <spencer.mike.r@gmail.com>
2018-04-30 06:46:07 +0100

    updated to run at home

--- grass_read.R
+++ grass_read.R
@@ -10,12 +10,12 @@ f1 = substr(f, 1, nchar(f) - 4)
 
 # shp
 lapply(1:length(f), function(i){
-   system(paste0("v.in.ogr -o input=", normalizePath("~"), "/Cloud/Michael/SRUC/hill_farms/data/spatial/", f[i], " layer=", f1[i], " output=", f1[i]))
+   system(paste0("v.in.ogr input=", normalizePath("~"), "/Cloud/Michael/SRUC/hill_farms/data/spatial/", f[i], " layer=", f1[i], " output=", f1[i]))
 })
 
 
 # geopackage
-system("v.in.ogr input=/home/mspencer/Cloud/Michael/SRUC/hill_farms/data/spatial/ag_parishes_2016.gpkg layer=original_parishes output=ag_parishes")
+system(paste0("v.in.ogr input=", normalizePath("~"), "/Cloud/Michael/SRUC/hill_farms/data/spatial/ag_parishes_2016.gpkg layer=original_parishes output=ag_parishes"))
 
 # raster
 system("r.in.gdal -o input=/home/mspencer/GIS/OS/Terrain50_Scotland.tif output=Terrain50_Scotland")
