94f9ba6feb3b80c30ef125259ed6f19aca6220fb
mikerspencer <spencer.mike.r@gmail.com>
2018-04-30 10:29:53 +0100

    missed a line for home...

--- grass_read.R
+++ grass_read.R
@@ -13,9 +13,8 @@ lapply(1:length(f), function(i){
    system(paste0("v.in.ogr input=", normalizePath("~"), "/Cloud/Michael/SRUC/hill_farms/data/spatial/", f[i], " layer=", f1[i], " output=", f1[i]))
 })
 
-
 # geopackage
 system(paste0("v.in.ogr input=", normalizePath("~"), "/Cloud/Michael/SRUC/hill_farms/data/spatial/ag_parishes_2016.gpkg layer=original_parishes output=ag_parishes"))
 
 # raster
-system("r.in.gdal -o input=/home/mspencer/GIS/OS/Terrain50_Scotland.tif output=Terrain50_Scotland")
+system(paste0("r.in.gdal input=", normalizePath("~"), "/GIS/OS/Terrain50_Scotland.tif output=Terrain50_Scotland"))
