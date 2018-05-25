c5264cfe63340d59e5750c507d031363daad57b8
mikerspencer <spencer.mike.r@gmail.com>
2018-05-25 15:30:44 +0100

    public sector workers

--- read_census.R
+++ read_census.R
@@ -31,11 +31,21 @@ census_2001 = KS01 %>%
           pop_female_2001=`2001 Population: Females`,
           pop_working_all_2001=`All people aged 16 to 74 in employment`,
           pop_working_ag_2001=`Percentage of people aged 16 to 74 working in: Agriculture, hunting and forestry`,
-          pop_working_fish_2001= `Percentage of people aged 16 to 74 working in: Fishing`) %>% 
+          pop_working_fish_2001= `Percentage of people aged 16 to 74 working in: Fishing`,
+          pop_working_utilities=`Percentage of people aged 16 to 74 working in: Electricity, gas and water supply`,
+          pop_working_public=`Percentage of people aged 16 to 74 working in: Public administration and defence, social security`,
+          pop_working_ed=`Percentage of people aged 16 to 74 working in: Education`,
+          pop_working_health=`Percentage of people aged 16 to 74 working in: Health and social work`) %>% 
    mutate(pop_working_ag_2001=as.numeric(str_replace(pop_working_ag_2001, "-", "0")),
-          pop_working_fish_2001=as.numeric(str_replace(pop_working_fish_2001, "-", "0"))) %>% 
-   mutate(pop_working_land_2001=pop_working_ag_2001 + pop_working_fish_2001) %>% 
-   select(-pop_working_ag_2001, -pop_working_fish_2001)
+          pop_working_fish_2001=as.numeric(str_replace(pop_working_fish_2001, "-", "0")),
+          pop_working_utilities=as.numeric(str_replace(pop_working_utilities, "-", "0")),
+          pop_working_infra=as.numeric(str_replace(pop_working_infra, "-", "0")),
+          pop_working_public=as.numeric(str_replace(pop_working_public, "-", "0")),
+          pop_working_ed=as.numeric(str_replace(pop_working_ed, "-", "0")),
+          pop_working_health=as.numeric(str_replace(pop_working_health, "-", "0"))) %>% 
+   mutate(pop_working_land_2001=pop_working_ag_2001 + pop_working_fish_2001,
+          pop_working_service_2001=pop_working_utilities + pop_working_infra + pop_working_public + pop_working_ed + pop_working_health) %>% 
+   select(-pop_working_ag_2001, -pop_working_fish_2001, -pop_working_utilities, -pop_working_infra, -pop_working_public, -pop_working_ed, -pop_working_health)
 
 census_2011 = KS101SC %>% 
    inner_join(KS605SC) %>% 
@@ -44,11 +54,28 @@ census_2011 = KS101SC %>%
           pop_male_2011=Males,
           pop_female_2011=Females,
           pop_working_all_2011=`All people aged 16 to 74 in employment`,
-          pop_working_land_2011=`A. Agriculture, forestry and fishing`) %>% 
+          pop_working_land_2011=`A. Agriculture, forestry and fishing`,
+          pop_working_utility=`D. Electricity, gas, steam and air conditioning supply`,
+          pop_working_water=`E. Water supply, sewerage, waste management and remediation activities`,
+          pop_working_public=`O. Public administration and defence, compulsory social security`,
+          pop_working_ed=`P. Education`,
+          pop_working_health=`Q. Human health and social work activities`) %>% 
    mutate(pop_working_all_2011=gsub(",", "", pop_working_all_2011),
-          pop_working_land_2011=gsub(",", "", pop_working_land_2011)) %>% 
+          pop_working_land_2011=gsub(",", "", pop_working_land_2011),
+          pop_working_utility=gsub(",", "", pop_working_utility),
+          pop_working_water=gsub(",", "", pop_working_water),
+          pop_working_public=gsub(",", "", pop_working_public),
+          pop_working_ed=gsub(",", "", pop_working_ed),
+          pop_working_health=gsub(",", "", pop_working_health)) %>% 
    mutate(pop_working_all_2011=as.numeric(str_replace(pop_working_all_2011, "-", "0")),
-          pop_working_land_2011=as.numeric(str_replace(pop_working_land_2011, "-", "0")))
+          pop_working_land_2011=as.numeric(str_replace(pop_working_land_2011, "-", "0")),
+          pop_working_utility=as.numeric(str_replace(pop_working_utility, "-", "0")),
+          pop_working_water=as.numeric(str_replace(pop_working_water, "-", "0")),
+          pop_working_public=as.numeric(str_replace(pop_working_public, "-", "0")),
+          pop_working_ed=as.numeric(str_replace(pop_working_ed, "-", "0")),
+          pop_working_health=as.numeric(str_replace(pop_working_health, "-", "0"))) %>% 
+   mutate(pop_working_service_2011=pop_working_utility + pop_working_water + pop_working_public + pop_working_ed + pop_working_health) %>% 
+   select(-pop_working_utility, -pop_working_water, -pop_working_public, -pop_working_ed, -pop_working_health)
 
 
 # Get proportion according to parishes
