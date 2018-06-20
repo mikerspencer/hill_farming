5e435b514df45d2718f2c1b5ff544e342491e808
mikerspencer <spencer.mike.r@gmail.com>
2018-06-20 16:13:30 +0100

    removed spatial and now uses bdline data

--- Highlands-Islands_population.R
+++ Highlands-Islands_population.R
@@ -5,22 +5,10 @@
 # --------------------------
 # --------------------------
 
-library(rgdal)
-library(broom)
 library(tidyverse)
 library(RColorBrewer)
 
-parishes = readOGR(paste0(normalizePath("~"), "/Cloud/Michael/SRUC/hill_farms/data/spatial/ag_parishes_2016.gpkg"), "simplified_parishes")
-
-parishes$id = row.names(parishes)
-parishes = tidy(parishes) %>% 
-   left_join(parishes@data)
-
-restrictions = read_csv("~/Cloud/Michael/SRUC/hill_farms/data/spatial-processed/parish_restrictions.csv") %>% 
-   mutate_all(funs(replace(., is.na(.), 0)))
-
-parishes = parishes %>% 
-   left_join(restrictions, by=c("PARCode"="PARCode", "PARName"="PARName")) %>% 
+restrictions = read_csv("~/projects/hill_farming/data/parish_restrictions.csv") %>% 
    mutate_all(funs(replace(., is.na(.), 0)))
 
 
@@ -38,25 +26,22 @@ score = restrictions %>%
    group_by(PARCode) %>% 
    summarise(score=sum(value))
 
-parishes = parishes %>% 
-   left_join(score)
-
 
 # --------------------------
 # Population
 
 
-x = read_csv("~/Cloud/Michael/SRUC/hill_farms/data/census/pop_census.csv") %>% 
+x = read_csv("~/projects/hill_farming/pop_census.csv") %>% 
    mutate(prop_2011_land=pop_working_land_2011 / pop_working_all_2011) %>% 
    select(PARCode, prop_2011_land) %>% 
    left_join(score)
 
-y = read_csv("~/Cloud/Michael/SRUC/hill_farms/data/spatial/LA_lookup.csv") %>% 
-   filter(AUTHORITY=="Dumfries and Galloway" | AUTHORITY=="Scottish Borders") %>% 
+y = read_csv("~/projects/hill_farming/LA_lookup.csv") %>% 
+   filter(NAME=="Highland" | NAME=="Orkney Islands" | NAME=="Shetland Islands" | NAME=="Argyll and Bute" | NAME=="Na h-Eileanan an Iar") %>% 
    left_join(x)
 
-png("~/Cloud/Michael/SRUC/hill_farms/figures/pop_census_southern-uplands.png", width=1700, height=900, res=150)
-ggplot(y, aes(score, prop_2011_land, colour=AUTHORITY)) +
+png("~/projects/hill_farming/pop_census_Highlands-Islands.png", width=1700, height=900, res=150)
+ggplot(y, aes(score, prop_2011_land, colour=NAME)) +
    geom_point(size=3) +
    scale_color_brewer(palette="Set2") +
    theme_bw() +
@@ -69,10 +54,11 @@ ggplot(y, aes(score, prop_2011_land, colour=AUTHORITY)) +
         colour="")
 dev.off()
 
-png("~/Cloud/Michael/SRUC/hill_farms/figures/pop_census_southern-uplands_national-context.png", width=1700, height=900, res=150)
+png("~/projects/hill_farming/pop_census_Highlands-Islands_national-context.png", width=1700, height=900, res=150)
 ggplot(x, aes(score, prop_2011_land)) +
-   geom_point(size=3, colour="grey70") +
-   geom_point(data=y, aes(score, prop_2011_land, colour=AUTHORITY), size=3) +
+   geom_point(size=3, colour="grey70", shape=1) +
+   geom_point(data=y, aes(score, prop_2011_land, colour=NAME), size=3, shape=1, stroke=1.5) +
+   scale_shape(solid=F) +
    scale_color_brewer(palette="Set2") +
    theme_bw() +
    theme(line=element_blank(),
