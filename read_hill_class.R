7b64f7d9b966e70679fff3a70c50e183b10c6724
mikerspencer <spencer.mike.r@gmail.com>
2018-07-30 13:50:46 +0100

    fixed peat column name

--- read_hill_class.R
+++ read_hill_class.R
@@ -55,7 +55,7 @@ land_classes = read_csv("~/projects/hill_farming/data/LCA_SCOTLAND_parishes.csv"
 # Peat/carbon map
 Peat = read_csv("~/projects/hill_farming/data/PEAT_SCOTLAND_parishes.csv") %>% 
    rename(PARCode=b_PARCode, PARName=b_PARName) %>% 
-   filter(a_PRIMARY_LA %in% c("Peaty gleys", "Peaty gleyed podzols", "Scree", "Subalpine podzols", "Dystrophic blanket peat", "Alpine podzols", "Dystrophic basin peat", "Peaty rankers", "Undifferentiated basin peats", "Dystrophic semi-confined peat", "Rankers",  "Podzolic rankers", "Peaty alluvial soils", "Shingle", "Undifferentiated semi-confined peats", "Subalpine (Orohemiarctic) podzols", "Rock", "Peaty podzols", "Alpine (Oroarctic) podzols", "Alpine (Oroarctic) gleys", "Brown rankers")) %>% 
+   filter(a_COMPSOIL %in% c("Peaty gleys", "Peaty gleyed podzols", "Scree", "Subalpine podzols", "Dystrophic blanket peat", "Alpine podzols", "Dystrophic basin peat", "Peaty rankers", "Undifferentiated basin peats", "Dystrophic semi-confined peat", "Rankers",  "Podzolic rankers", "Peaty alluvial soils", "Shingle", "Undifferentiated semi-confined peats", "Subalpine (Orohemiarctic) podzols", "Rock", "Peaty podzols", "Alpine (Oroarctic) podzols", "Alpine (Oroarctic) gleys", "Brown rankers")) %>% 
    group_by(PARCode, PARName) %>% 
    summarise(peat_prop=sum(PEAT_SCOTLAND_area / b_Shape_Area))
 
