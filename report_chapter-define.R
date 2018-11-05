c797324ecdfaab9b6f0ce0afee18f156c13ea437
mikerspencer <spencer.mike.r@gmail.com>
2018-11-05 17:02:30 +0000

    terrain50 and grazing figures

--- report_chapter-define.R
+++ report_chapter-define.R
@@ -47,42 +47,86 @@ plot.map = function(i, tit){
       theme_minimal() +
       theme(axis.text=element_blank(),
             axis.title=element_blank(),
-            line=element_blank())
+            line=element_blank(),
+            text=element_text(size=25))
 }
 
 
 # ---------------------------------------------
 # Terrain50
 
+elev = readGDAL(paste0(normalizePath("~"), "/Cloud/Michael/SRUC/hill_farms/data/spatial-processed/elev.tif")) %>% 
+   tidy()
+
+slope = readGDAL(paste0(normalizePath("~"), "/Cloud/Michael/SRUC/hill_farms/data/spatial-processed/slope.tif")) %>% 
+   as.data.frame()
+
 # Elevation
 
+png("~/Cloud/Michael/SRUC/hill_farms/report/Figures/elev.png",
+    height=1080, width=800)
+ggplot(elev, aes(x, y, fill=band1)) +
+   geom_raster() +
+   geom_polygon(data=Scotland, aes(long, lat, group=group),
+                colour="grey30", fill=NA, size=0.1) +
+   coord_equal() +
+   scale_fill_distiller(palette="Greens", direction=1) +
+   labs(fill="Elevation\n(metres)") +
+   theme_minimal() +
+   theme(axis.text=element_blank(),
+         axis.title=element_blank(),
+         line=element_blank(),
+         text=element_text(size=25))
+dev.off()
 
 # Slope
+png("~/Cloud/Michael/SRUC/hill_farms/report/Figures/slope.png",
+    height=1080, width=800)
+ggplot(slope, aes(x, y, fill=band1)) +
+   geom_raster() +
+   geom_polygon(data=Scotland, aes(long, lat, group=group),
+                colour="grey30", fill=NA, size=0.1) +
+   coord_equal() +
+   scale_fill_distiller(palette="Greens", direction=1) +
+   labs(fill="Slope\n(degrees)") +
+   theme_minimal() +
+   theme(axis.text=element_blank(),
+         axis.title=element_blank(),
+         line=element_blank(),
+         text=element_text(size=25))
+dev.off()
 
 
 # ---------------------------------------------
 # Common grazing
 
+png("~/Cloud/Michael/SRUC/hill_farms/report/Figures/common_grazing.png",
+    height=1080, width=800)
 plot.map("c_grazing_prop", "Common\ngrazing\nproportion")
-
+dev.off()
 
 
 # ---------------------------------------------
 # Rough grazing
 
+png("~/Cloud/Michael/SRUC/hill_farms/report/Figures/rough_grazing.png",
+    height=1080, width=800)
 plot.map("r_grazing_prop", "Rough\ngrazing\nproportion")
-
+dev.off()
 
 # ---------------------------------------------
 # Landscape character (SNH)
+
 
 
 # ---------------------------------------------
 # Carbon and peat (SNH)
 
 
+
 # ---------------------------------------------
 # Land capability (JHI)
+
 
 
 # ---------------------------------------------
