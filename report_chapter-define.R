72bdd338609762030662b1ad90914c4e56fa04a4
mikerspencer <spencer.mike.r@gmail.com>
2018-11-06 12:59:57 +0000

    lca figure and peat figure framework

--- report_chapter-define.R
+++ report_chapter-define.R
@@ -114,14 +114,91 @@ png("~/Cloud/Michael/SRUC/hill_farms/report/Figures/rough_grazing.png",
 plot.map("r_grazing_prop", "Rough\ngrazing\nproportion")
 dev.off()
 
+
 # ---------------------------------------------
 # Landscape character (SNH)
 
+lca = readOGR(paste0(normalizePath("~"), "/Cloud/Michael/SRUC/hill_farms/data/spatial"), "LCA_SCOTLAND")
+
+y = c("Flat or Rolling, Smooth or Sweeping, Extensive, High Moorlands of the Highlands and Islands",
+      "Inland Loch",
+      "Highland Straths",
+      "Moorland Transitional Landscapes of the Highlands and Islands",
+      "High, Massive, Rolling, Rounded Mountains of the Highlands and Islands",
+      "High Massive Mountain Plateau of the Cairngorms",
+      "Smooth Upland Moorland Hills",
+      "Highland Foothills",
+      "Upland Igneous and Volcanic Hills The Ochil, Sidlaw, Cleish and Lomond Hills",
+      "High, Massive, Rugged, Steep-Sided Mountains of the Highlands and Islands",
+      "Sea Lochs of the Highlands and Islands",
+      "Highland and Island Rocky Coastal Landscapes",
+      "Peatland Landscapes  of the Highlands and Islands",
+      "Rocky Moorlands of the Highlands and Islands",
+      "Rugged, Craggy Upland Hills and Moorlands of the Highlands, including the Trossachs",
+      "Low Coastal Hills of the Highlands and Islands",
+      "Coastal Hills Headlands Plateaux and Moorlands",
+      "Lowland Hills",
+      "Foothills and Pronounced Hills",
+      "Upland Hills, The Southern Uplands and Cheviots",
+      "High Plateau Moorlands",
+      "Rugged Granite Uplands",
+      "Rocky Volcanic Islands",
+      "Rugged Moorland Hills",
+      "Upland Fringe Moorland",
+      "Upland Hills, The Lammemuir, Pentland and Moorfoot Hills",
+      "Rocky Coasts Cliffs and Braes of the Lowlands",
+      "Knock or Rock and Lochan of the Islands",
+      "Highland Cnocan")
+
+lca$hill[lca$LEVEL_3 %in% y] = "Hill"
+lca$hill[!lca$LEVEL_3 %in% y] = "Other"
+
+lca$id = row.names(lca)
+lca = tidy(lca) %>% 
+   left_join(lca@data)
+
+png("~/Cloud/Michael/SRUC/hill_farms/report/Figures/landscape.png",
+    height=1080, width=800)
+ggplot(lca, aes(long, lat, group=group)) +
+   geom_polygon(aes(fill=hill)) +
+   geom_polygon(data=Scotland, aes(long, lat, group=group),
+                colour="grey30", fill=NA, size=0.1) +
+   coord_equal() +
+   scale_fill_brewer(palette="Set2") +
+   labs(fill="Landscape\nclass") +
+   theme_minimal() +
+   theme(axis.text=element_blank(),
+         axis.title=element_blank(),
+         line=element_blank(),
+         text=element_text(size=25))
+dev.off()
 
 
 # ---------------------------------------------
 # Carbon and peat (SNH)
 
+lca$hill[lca$LEVEL_3 %in% y] = "Peat"
+lca$hill[!lca$LEVEL_3 %in% y] = "Other"
+
+lca$id = row.names(lca)
+lca = tidy(lca) %>% 
+   left_join(lca@data)
+
+png("~/Cloud/Michael/SRUC/hill_farms/report/Figures/peat.png",
+    height=1080, width=800)
+ggplot(lca, aes(long, lat, group=group)) +
+   geom_polygon(aes(fill=hill)) +
+   geom_polygon(data=Scotland, aes(long, lat, group=group),
+                colour="grey30", fill=NA, size=0.1) +
+   coord_equal() +
+   scale_fill_brewer(palette="Set3", direction=-1) +
+   labs(fill="Soil") +
+   theme_minimal() +
+   theme(axis.text=element_blank(),
+         axis.title=element_blank(),
+         line=element_blank(),
+         text=element_text(size=25))
+dev.off()
 
 
 # ---------------------------------------------
